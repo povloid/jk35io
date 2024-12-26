f7804d8d72fc8077716d886523e2ec5bfcfe5add
Pavel Kopychenko <t34box@gmail.com>
2024-12-26 22:34:22 +0300

    md5sum digest fix.

--- src/main/java/k35/io/util/FilesTools.java
+++ src/main/java/k35/io/util/FilesTools.java
@@ -2,7 +2,6 @@ package k35.io.util;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
@@ -16,7 +15,6 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,108 +23,115 @@ import org.slf4j.LoggerFactory;
  */
 public class FilesTools {
 
-	static final Logger log = LoggerFactory.getLogger(FilesTools.class);
+  static final Logger log = LoggerFactory.getLogger(FilesTools.class);
 
-	/**
-	 * Что бы не могли создать экземпляр, так как тут мы будем использовать только
-	 * статические методы
-	 */
-	private FilesTools() {
+  /**
+   * Что бы не могли создать экземпляр, так как тут мы будем использовать только статические методы
+   */
+  private FilesTools() {
 
-	}
+  }
 
-	/**
-	 * Загрузить файл в строку по пути из класса
-	 * 
-	 * @param clazz
-	 * @param path
-	 * @return
-	 * @throws IOException
-	 */
-	public static final String loadStringFromFile(Class<?> clazz, String path) throws IOException {
-		return new String(clazz.getResourceAsStream(path).readAllBytes(), StandardCharsets.UTF_8);
-	}
+  /**
+   * Загрузить файл в строку по пути из класса
+   * 
+   * @param clazz
+   * @param path
+   * @return
+   * @throws IOException
+   */
+  public static final String loadStringFromFile(Class<?> clazz, String path) throws IOException {
+    return new String(clazz.getResourceAsStream(path).readAllBytes(), StandardCharsets.UTF_8);
+  }
 
-	/**
-	 * Вычислить контрольную сумму файла по пути
-	 * 
-	 * @param path
-	 * @return
-	 * @throws IOException
-	 * @throws NoSuchAlgorithmException
-	 */
-	public static String calculateFileMd5sum(Path path) throws IOException, NoSuchAlgorithmException {
-		log.info("Вычисление контрольной суммы (MD5) файла по пути {}", path);
+  /**
+   * Вычислить контрольную сумму файла по пути
+   * 
+   * @param path
+   * @return
+   * @throws IOException
+   * @throws NoSuchAlgorithmException
+   */
+  public static String calculateFileMd5sum(Path path) throws IOException, NoSuchAlgorithmException {
+    log.info("Вычисление контрольной суммы (MD5) файла по пути {}", path);
 
-		final var md = MessageDigest.getInstance("MD5");
+    final var md = MessageDigest.getInstance("MD5");
 
-		final var buffer = new byte[1024 * 64];
+    final var buffer = new byte[1024 * 64];
 
-		try (final var is = new FileInputStream(path.toString()); final var dis = new DigestInputStream(is, md)) {
+    try (final var is = new FileInputStream(path.toString());
+        final var dis = new DigestInputStream(is, md)) {
 
-			while (dis.read(buffer) != -1) {
-				; // empty loop to clear the data
-			}
+      while (dis.read(buffer) != -1) {
+        ; // empty loop to clear the data
+      }
 
-			final var hash = dis.getMessageDigest().digest();
-			String md5sum = new BigInteger(1, hash).toString(16);
+      final var digest = dis.getMessageDigest().digest();
 
-			log.info("Контрольная сумма (MD5) файла по пути {} ровна {}", path, md5sum);
 
-			return md5sum;
+      final StringBuffer sb = new StringBuffer();
+      for (final byte b : digest) {
+        sb.append(String.format("%02x", b & 0xff));
+      }
 
-		}
+      final String md5sum = sb.toString();
 
-	}
+      log.info("Контрольная сумма (MD5) файла по пути {} ровна {}", path, md5sum);
 
-	/**
-	 * Получить расширение файла
-	 * 
-	 * @param path
-	 * @return
-	 */
-	public static Optional<String> getFileExtension(Path path) {
-		final var fileName = path.getFileName().toString();
-		return getFileExtension(fileName);
-	}
+      return md5sum;
 
-	/**
-	 * Получить расширение файла
-	 * 
-	 * @param path
-	 * @return
-	 */
-	public static Optional<String> getFileExtension(String fileName) {
-		final int dotIndex = fileName.lastIndexOf('.');
-		return (dotIndex == -1) ? Optional.empty() : Optional.of(fileName.substring(dotIndex + 1));
-	}
+    }
 
-	/**
-	 * Получить список файлов директории
-	 * 
-	 * @param dir
-	 * @return
-	 * @throws IOException
-	 */
-	public static Set<String> listFilesUsingFilesList(String dir) throws IOException {
-		try (Stream<Path> stream = Files.list(Paths.get(dir))) {
-			return stream.filter(file -> !Files.isDirectory(file)).map(Path::getFileName).map(Path::toString)
-					.collect(Collectors.toSet());
-		}
-	}
+  }
 
-	/**
-	 * Получить список файлов с проходом в глубину
-	 * 
-	 * @param dir
-	 * @param depth
-	 * @return
-	 * @throws IOException
-	 */
-	public static List<Path> listFilesUsingFileWalk(String dir, int depth) throws IOException {
-		try (Stream<Path> stream = Files.walk(Paths.get(dir), depth, FileVisitOption.FOLLOW_LINKS)) {
-			return stream.filter(file -> !Files.isDirectory(file)).collect(Collectors.toList());
-		}
-	}
+  /**
+   * Получить расширение файла
+   * 
+   * @param path
+   * @return
+   */
+  public static Optional<String> getFileExtension(Path path) {
+    final var fileName = path.getFileName().toString();
+    return getFileExtension(fileName);
+  }
+
+  /**
+   * Получить расширение файла
+   * 
+   * @param path
+   * @return
+   */
+  public static Optional<String> getFileExtension(String fileName) {
+    final int dotIndex = fileName.lastIndexOf('.');
+    return (dotIndex == -1) ? Optional.empty() : Optional.of(fileName.substring(dotIndex + 1));
+  }
+
+  /**
+   * Получить список файлов директории
+   * 
+   * @param dir
+   * @return
+   * @throws IOException
+   */
+  public static Set<String> listFilesUsingFilesList(String dir) throws IOException {
+    try (Stream<Path> stream = Files.list(Paths.get(dir))) {
+      return stream.filter(file -> !Files.isDirectory(file)).map(Path::getFileName)
+          .map(Path::toString).collect(Collectors.toSet());
+    }
+  }
+
+  /**
+   * Получить список файлов с проходом в глубину
+   * 
+   * @param dir
+   * @param depth
+   * @return
+   * @throws IOException
+   */
+  public static List<Path> listFilesUsingFileWalk(String dir, int depth) throws IOException {
+    try (Stream<Path> stream = Files.walk(Paths.get(dir), depth, FileVisitOption.FOLLOW_LINKS)) {
+      return stream.filter(file -> !Files.isDirectory(file)).collect(Collectors.toList());
+    }
+  }
 
 }
